4c049c1edeb48ed071a601e04ed58333080cbc59
hellum55 <hellum.christian@gmail.com>
2023-11-28 14:55:55 +0100

    d

---
+++ Exercise2.R
@@ -0,0 +1,84 @@
+#Question 1:####
+library(shiny)
+server1 <- function(input, output, session) {
+  c <- reactive(input$a + input$b)
+  e <- reactive(c() + input$d)
+  output$f <- renderText(e())
+}
+server2 <- function(input, output, session) {
+  x <- reactive(input$x1 + input$x2 + input$x3)
+  y <- reactive(input$y1 + input$y2)
+  output$z <- renderText(x() / y())
+}
+server3 <- function(input, output, session) {
+  d <- reactive(c() ^ input$d)
+  a <- reactive(input$a * 10)
+  c <- reactive(b() / input$c)
+  b <- reactive(a() + input$b)
+}
+
+#Question 2:####
+#Why will this code fail?
+var <- reactive(df[input$var])
+range <- reactive(range(var(), na.rm = TRUE))
+#It cannot react to its own expression
+
+#Question 3:###
+ui <- fluidPage(
+  dataTableOutput("table")
+)
+server <- function(input, output, session) {
+  output$table <- renderDataTable(mtcars, options = list(pageLength = 5,
+                                                         searching = FALSE,
+                                                         ordering = FALSE,
+                                                         info = FALSE))
+}
+shinyApp(ui, server)
+
+#Question 4:####
+ui <- fluidPage(
+  titlePanel("Central limit theorem"),
+  sidebarLayout(
+    sidebarPanel(
+      numericInput("m", "Number of samples:", 2, min = 1, max = 100)
+    ),
+    mainPanel(
+      plotOutput("hist"),
+      position = c("left")
+    )
+  )
+)
+server <- function(input, output, session) {
+  output$hist <- renderPlot({
+    means <- replicate(1e4, mean(runif(input$m)))
+    hist(means, breaks = 20)
+  }, res = 96)
+}
+shinyApp(ui, server)
+
+#Question 5:####
+install.packages("shinythemes")
+library(shinythemes)
+ui <- fluidPage(
+  theme = bslib::bs_theme(bootswatch = "superhero"),
+  titlePanel("Central limit theorem"),
+  sidebarLayout(
+    sidebarPanel(
+      numericInput("m", "Number of samples:", 2, min = 1, max = 100)
+    ),
+    mainPanel(
+      plotOutput("hist"),
+      position = c("left")
+    )
+  )
+)
+server <- function(input, output, session) {
+  output$hist <- renderPlot({
+    means <- replicate(1e4, mean(runif(input$m)))
+    hist(means, breaks = 20)
+  }, res = 96)
+}
+shinyApp(ui, server)
+
+#Question 6:####
+
